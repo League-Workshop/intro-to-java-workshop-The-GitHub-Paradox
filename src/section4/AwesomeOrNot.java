b5b8d3e6d8f37b3f3dbe059964692f61550e4713
League Student <league@iMac-15.attlocal.net>
2020-02-15 15:53:34 -0800

    Dank

--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -2,10 +2,29 @@ package section4;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes all the steps below…
-
+	public static void main(String[] args) {
+		Random random = new Random ();
+		int randomNumber = random.nextInt(4);
+		JOptionPane.showMessageDialog(null, + randomNumber);
+		String owo = JOptionPane.showInputDialog("What do u think is awesome. This is 100% not a stalker website that will track what you answer lol. Totally not a scam");
+		if(randomNumber==0) {
+			JOptionPane.showMessageDialog(null, "I think that " + owo + " is dank");
+		}
+		if(randomNumber==1) {
+			JOptionPane.showMessageDialog(null, "I think that " + owo + " is meh");
+		}
+		if(randomNumber==2) {
+			JOptionPane.showMessageDialog(null, "I think that " + owo + " is baff");
+		}
+		if(randomNumber==3) {
+			JOptionPane.showMessageDialog(null, "LOL NORMIE");
+		}
+	}
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
 
 	// 3. Print out this variable
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,15 +1,37 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int score = 0;
 		// 2.  Ask the user a question 
+		String Q1 = JOptionPane.showInputDialog("Omega Shaggy or Shrek?");
+		String Q2 = JOptionPane.showInputDialog("The Ting goes skrrrrrrta. Whomst'de say'ed dis MLG fhrase?");
+		String Q3 = JOptionPane.showInputDialog("Ah yes ________ meme man");
+		String Q4 = JOptionPane.showInputDialog("Dunked on");
+		String Q5 = JOptionPane.showInputDialog("-1 point m8? (y/n)");
 		
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if(Q1.equalsIgnoreCase("Yes")) {
+			score++;
+		}
+		if(Q2.equalsIgnoreCase("Big Shaq")) {
+			score++;
+		}
+		if(Q3.equalsIgnoreCase("enslaved")) {
+			score++;
+		}
+		if(Q4.equalsIgnoreCase("Sanesss")) {
+			score++;
+		}
+		if(Q5.equalsIgnoreCase("y")) {
+			score--;
+		}
+		JOptionPane.showMessageDialog(null, "Wow u suck your score is " + score);
 		// 4.  if the user's answer was correct, add one to their score 
 		
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
